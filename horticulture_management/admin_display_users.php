94fd44147e150957c1f9eea66983a0e1a9d422b0
RochelleV22 <mca.1960@unigoa.ac.in>
2020-12-21 16:34:25 +0530

    Horticulture Management System

---
+++ horticulture_management/admin_display_users.php
@@ -0,0 +1,34 @@
+<?php
+include("connect.php");
+	$query = "SELECT * FROM users";
+	echo "<b><h1>ADMIN PANEL</h1></b>";
+	echo "<b><h2>REGISTERED USERS</h2></b>";
+	$result = mysqli_query($conn, $query);
+	if(mysqli_num_rows($result) > 0) {
+		echo "<table>
+			<tr> 
+			<th>Username</th>
+			<th>Password</th>
+			<th>Usertype</th>
+			</tr>";
+		while ($row = mysqli_fetch_assoc($result)){
+			echo "<tr> 
+			<td>".$row["username"]."</td>
+			<td>".$row["password"]."</td>
+			<td>".$row["usertype"]."</td>
+			</tr>";
+		}
+		echo "</table></b>";
+	}
+	else
+		echo "<b>NO RESULT</b>";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADMIN</title>
+</head>
+<body bgcolor="#9999ff">
+	<br><a href="admin.php">GO BACK TO HOMEPAGE</a>
+</body>
+</html>
---
+++ horticulture_management/user.php
@@ -0,0 +1,20 @@
+<?php
+include("connect.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>USER</title>
+	<h1>USER PANEL</h1>
+</head>
+<body bgcolor="orange">
+	<div>
+		<ul>
+			<a href="user_view.php"><input type="submit" value="VIEW PRODUCTS"></a><br><br>
+			<a href="user_search.php"><input type="submit" value="SEARCH PRODUCT"></a><br><br>
+			<a href="order_product.php"><input type="submit" value="ORDER PRODUCT"></a><br><br>
+			<a href="cancel_product.php"><input type="submit" value="CANCEL ORDER"></a><br><br>
+		</ul>
+	</div>
+</body>
+</html>
